3805f285cb3103a9582804dd5a9fe9e15cab426a
Gunawardhane <yasangawd@gmail.com>
2023-06-27 22:16:07 +0530

    change

---
+++ Assignments/JS/SPA/POS/db/DB.js
@@ -0,0 +1,16 @@
+let customerDB = [
+    setCustomer("C00-001","Bandara","Galle",20000)
+    
+];
+
+let itemDB = [
+    setItem("ITM-001","Rice",180,50)
+  
+];
+
+let cartDB = [];
+
+let ordersDB = [
+   
+    
+];
--- Assignments/JS/SPA/model/Customer.js
+++
@@ -1,8 +0,0 @@
-function setCustomer(customerId, name, address, salary) {
-    return {
-        cusId: customerId,
-        cusName: name,
-        cusAddress: address,
-        cusSalary: salary
-    };
-}
